7cb808c618064b29569d41a78e83b1d740becf7a
Attila Szőke <clownface68@gmail.com>
2023-02-20 21:43:00 +0100

    Added some interactivity

--- src/components/Svelte.svelte
+++ src/components/Svelte.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import ctl from '@netlify/classnames-template-literals';
 	export let msg: string;
+
+	let count = 1;
 </script>
 
-<div class={ctl('msg text-xl')}>I am a Svelte component! - {msg}</div>
+<div class={ctl('msg text-xl')}>
+	I am a Svelte component! - {msg} - {count}
+	<button
+		class={ctl('rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700')}
+		on:click={() => count++}>Increase Svelte counter</button
+	>
+</div>
 
 <style lang="scss">
 	.msg {
--- src/components/react/ReactTs.tsx
+++ src/components/react/ReactTs.tsx
@@ -1,5 +1,19 @@
+import { useState } from 'react';
 import './styles.scss';
 
-const React = () => <div className="msg text-lg">I am a React component with TypeScript!</div>;
+const React = () => {
+	const [count, setCount] = useState(0);
+	return (
+		<div className="msg text-lg">
+			I am a React component with TypeScript! - {count}
+			<button
+				className="rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700"
+				onClick={() => setCount(count + 1)}
+			>
+				Increase React counter
+			</button>
+		</div>
+	);
+};
 
 export default React;
